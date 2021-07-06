1411632374f79382323d75503980bfbdd3b63cab
Corey Mills <43578746+CoreyMills@users.noreply.github.com>
2021-07-06 19:21:42 +0100

    update
    test touchstart

--- NavMenu.js
+++ NavMenu.js
@@ -1,6 +1,15 @@
 window.addEventListener('resize',function(){UpdateNav();});
 window.addEventListener('load',function(){UpdateNav();});
+//window.addEventListener('click',function(){FlipDropDownState(false);});
+document.onreadystatechange=function(){
+    //if (document.readyState==='complete'){
+		//$('.dropDownBtn').each(function(){$(this)[0].addEventListener('click',function(){FlipDropDownState(true);});});
+	//}
+}
 function UpdateNav(){
 	if($(window).width()<1100){$('#mainNav').hide();$('#ddNav').show();} 
 	else {$('#mainNav').show();$('#ddNav').hide();}
 }
+$('dropDownContent').bind('touchstart', function() {});
+
+//function FlipDropDownState(val){if(val == true){$('.dropDownContent')[0].style.display="block";}else{alert("test");$('.dropDownContent')[0].style.display="none";}}
