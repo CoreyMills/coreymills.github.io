3469be9586c1571ff767d38b2d2bf19ebf7c3707
Corey Mills <43578746+CoreyMills@users.noreply.github.com>
2021-07-06 21:30:57 +0100

    fixing media container sizes

--- NavMenu.js
+++ NavMenu.js
@@ -1,13 +1,13 @@
 window.addEventListener('resize',function(){UpdateNav();});
-window.addEventListener('load',function(){UpdateNav();});
+window.addEventListener('scroll',function(){$('.dropDownContent')[0].style.display="none";})
+document.onreadystatechange=function(e){if (document.readyState === 'complete'){UpdateNav();}}
+
 function UpdateNav(){
 	if($(window).width()<1100){$('#mainNav').hide();$('#ddNav').show();} 
 	else {$('#mainNav').show();$('#ddNav').hide();}
 }
 function FlipDropDownState(){
 	var dropDownC = $('.dropDownContent'); 
-	if(dropDownC.is(":visible")){
-		dropDownC[0].style.display="none";}
-	else{
-		dropDownC[0].style.display="block";}
+	if(dropDownC.is(":visible")){dropDownC[0].style.display="none";}
+	else{dropDownC[0].style.display="block";}
 }
