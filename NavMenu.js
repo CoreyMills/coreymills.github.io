c23783af51748f02d64e29cd643d6b81b3a01fbd
Corey Mills <43578746+CoreyMills@users.noreply.github.com>
2021-07-06 20:25:01 +0100

    touchstart complete

--- NavMenu.js
+++ NavMenu.js
@@ -1,10 +1,5 @@
 window.addEventListener('resize',function(){UpdateNav();});
 window.addEventListener('load',function(){UpdateNav();});
-window.addEventListener('load',function(){UpdateNav();});
-
-function Init(){
-	$('dropDownBtn').each(function(){.addEventListener('touchstart', process_touchstart, false);});
-}
 function UpdateNav(){
 	if($(window).width()<1100){$('#mainNav').hide();$('#ddNav').show();} 
 	else {$('#mainNav').show();$('#ddNav').hide();}
