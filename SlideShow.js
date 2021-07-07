df19618e02658d07a891011af9159f2d1a4e8e0c
Corey Mills <43578746+CoreyMills@users.noreply.github.com>
2021-07-08 00:22:50 +0100

    updated slideshow and videos
    improved slideshow to not require specific ids.
    
    placed video attributes in html

--- SlideShow.js
+++ SlideShow.js
@@ -3,34 +3,37 @@ document.onreadystatechange=function(){
     if (document.readyState==='complete'){
 		let containers=document.getElementsByClassName("ssContainer");
 		for(let i=0;i<containers.length;i++){
-			let key = "ss" + i;
+			let key = i
 			slideMap.set(key,0);
-			let slides = document.getElementsByClassName(key);
-			if(slides.length>1){
-				let pBtn=document.createElement("a");pBtn.className="prev button";pBtn.onclick=function(){ChangeSlide(-1,key)};pBtn.innerText='\u276E';
-				containers[i].appendChild(pBtn);
-				
-				let nBtn=document.createElement("a");nBtn.className="next button";nBtn.onclick=function(){ChangeSlide(1,key)};nBtn.innerText='\u276F';
-				containers[i].appendChild(nBtn);
+			let slides = containers[i].getElementsByClassName('ssMedia');
+			if(slides.length==0){continue;}
+			else{
+				for(let j=0;j<slides.length;j++){slides[j].style.display="none";}
+				if(slides.length>1){
+					let pBtn=document.createElement("a");pBtn.className="prev button";pBtn.onclick=function(){ChangeSlide(-1,key)};pBtn.innerText='\u276E';
+					let nBtn=document.createElement("a");nBtn.className="next button";nBtn.onclick=function(){ChangeSlide(1,key)};nBtn.innerText='\u276F';
+					containers[i].appendChild(pBtn);
+					containers[i].appendChild(nBtn);
+				}
 			}
-			ShowSlides(slideMap.get(key), key);
+			ShowSlides(0,0,key);
 			containers[i].style.display = "block";
 		}
-		$('video').each(function(){$(this)[0].muted=true;$(this)[0].controls=true;$(this)[0].loop=true;$(this)[0].preload="none";$(this)[0].attr('webkit-playsinline', '');});
 		CheckVisibility();
 	}
 }
 function ChangeSlide(n,key){
-	slideMap.set(key,slideMap.get(key)+n);
-	ShowSlides(slideMap.get(key),key);
+	let prevIndex=slideMap.get(key);
+	slideMap.set(key,prevIndex+n);
+	ShowSlides(slideMap.get(key), prevIndex,key);
 	CheckVisibility();
 }
-function ShowSlides(index,key){
-	let slides=document.getElementsByClassName(key);
-	if(index>=slides.length){slideMap.set(key,0);}
-	if(index<0){slideMap.set(key,slides.length-1);}
-	for(let i=0;i<slides.length;i++){slides[i].style.display = "none";}
-	slides[slideMap.get(key)].style.display = "block";
+function ShowSlides(newIndex,prevIndex,key){
+	let slides=document.getElementsByClassName("ssContainer")[key].getElementsByClassName('ssMedia');
+	if(newIndex>=slides.length){slideMap.set(key,0);}
+	if(newIndex<0){slideMap.set(key,slides.length-1);}
+	slides[prevIndex].style.display="none";
+	slides[slideMap.get(key)].style.display="block";
 }
 $(window).scroll(function(){CheckVisibility();});
 function CheckVisibility(){
@@ -48,7 +51,7 @@ function IsVisible(vid){
 	let rectInfo=vid.getBoundingClientRect();let percentH=rectInfo.height*0.2;
 	return(rectInfo.width!=0&&rectInfo.top+percentH>headerH&&rectInfo.bottom-percentH<(windowH-footerH))?true:false;
 }
-function PlayVideo(vid){setTimeout(function(){vid.play();},200);}
+function PlayVideo(vid){vid.muted=true;setTimeout(function(){vid.play();},200);}
 function PauseVideo(vid){vid.pause();}
 function PlayIFrame(vid){setTimeout(function(){vid.contentWindow.postMessage('{"event":"command","func":"'+'playVideo'+'","args":""}','*');},200);}
 function PauseIFrame(vid){vid.contentWindow.postMessage('{"event":"command","func":"'+'pauseVideo'+'","args":""}','*');}
